9dc05b08bd1c449115d1dba7b10ff52cd594a55c
John Arnulfo Navera <navera.johnarnulfo@gmail.com>
2020-12-17 09:32:53 +0800

    created the basic design template

--- index.php
+++ index.php
@@ -1,1 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css">		
+		<title>To Do List</title>
+	</head>
+	<body>
+		<h1 style="font-size: 50px;">To Do List</h1>
+		
+		<div class="container">
+			<div class="tabContainer">
+				<div class="tab">
+					<-
+				</div>
+				<div class="tab">
+					Today
+				</div>	
+				<div class="tab">
+					->
+				</div>
+			</div>
 
+			<div class="taskBox">
+				<div class="task" style="width: 88%;">
+					This is a textbox
+				</div>
+				<div class="button">
+					Enter
+				</div>
+			</div>
+
+			<div class="taskBox">
+				<div class="task">
+					This is a task
+				</div>
+				<div class="buttonContainer">
+					<div class="button">
+						/
+					</div>
+					<div class="button">
+						E
+					</div>
+					<div class="button">
+						X
+					</div>
+				</div>
+
+				<div class="arrow">
+					L>
+				</div>
+				<div class="subTaskBox">
+					<div class="task">
+						This is a subtask
+					</div>
+					<div class="buttonContainer">
+						<div class="button">
+							/
+						</div>
+						<div class="button">
+							E
+						</div>
+						<div class="button">
+							X
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
