3df1d69ab2b1c601ce317e8bef99e863e026c516
John Arnulfo Navera <navera.johnarnulfo@gmail.com>
2021-01-05 14:42:34 +0800

    funtion: subtask complete button

--- complete.php
+++ complete.php
@@ -1,16 +1,38 @@
 <?php
+	session_start();
+
 	include_once "db.php";
 
 	if($_GET['id'])
 	{
 		$id = $_GET['id'];
 
-		$result = $conn->query("UPDATE task SET status=1 WHERE taskno=".$id);
+		$check = $conn->query("SELECT * FROM subtask WHERE status=0 AND mainTask=".$id);
+		
+		if(!mysqli_num_rows($check))
+		{
+			$conn->query("UPDATE task SET status=1 WHERE taskno=".$id);
 
-		header("location: index.php");
+			$_SESSION['error'] = 0;
+			$_SESSION['errorMessege'] = "";
+			header("location: index.php");
+		}
+		else
+		{
+			$_SESSION['error'] = $id;
+			$_SESSION['errorMessege'] = "Complete subtask first";
+			header("location: index.php?error");
+		}
+
 	}
 	else
 	{
-		
+		$sub = $_GET['sub'];
+
+		$conn->query("UPDATE subtask SET status=1 WHERE subtaskno=".$sub);
+
+		$_SESSION['error'] = 0;
+		$_SESSION['errorMessege'] = "";
+		header("location: index.php");
 	}
 ?>
