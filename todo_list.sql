4c866beff8d437ccf9c7e40b7d270289e8b5ef15
John Arnulfo Navera <navera.johnarnulfo@gmail.com>
2021-01-05 15:46:46 +0800

    clear database

--- todo_list.sql
+++ todo_list.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 05, 2021 at 08:37 AM
+-- Generation Time: Jan 05, 2021 at 08:45 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -32,23 +32,8 @@ CREATE TABLE `subtask` (
   `mainTask` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `date` date NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `status` bit(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `subtask`
---
-
-INSERT INTO `subtask` (`subtaskno`, `mainTask`, `name`, `date`, `status`) VALUES
-(1, 1, 'Programming', '2020-12-17', 0),
-(2, 1, 'Database', '2020-12-17', 0),
-(3, 3, 'Morning', '2020-12-28', 1),
-(4, 4, 'breakfast', '2020-12-28', 0),
-(5, 4, 'lunch', '2020-12-28', 0),
-(6, 5, 'Database', '2020-12-28', 0),
-(7, 5, 'Method', '2020-12-28', 0),
-(14, 10, 'ok', '2021-01-05', 1),
-(15, 11, 'Eat', '2021-01-05', 0);
 
 -- --------------------------------------------------------
 
@@ -60,21 +45,8 @@ CREATE TABLE `task` (
   `taskno` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `date` date NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `status` bit(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `task`
---
-
-INSERT INTO `task` (`taskno`, `name`, `date`, `status`) VALUES
-(1, 'Study', '2020-12-17', 0),
-(2, 'Shower', '2020-12-17', 0),
-(3, 'Be Lazy', '2020-12-28', 1),
-(4, 'Eat', '2020-12-28', 0),
-(5, 'Study', '2020-12-28', 0),
-(10, 'Sleep', '2021-01-05', 1),
-(11, 'Be Lazy', '2021-01-05', 0);
 
 --
 -- Indexes for dumped tables
@@ -101,13 +73,13 @@ ALTER TABLE `task`
 -- AUTO_INCREMENT for table `subtask`
 --
 ALTER TABLE `subtask`
-  MODIFY `subtaskno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `subtaskno` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `task`
 --
 ALTER TABLE `task`
-  MODIFY `taskno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `taskno` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
